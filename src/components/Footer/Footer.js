9de0b141fc399c47e5d66f5fca4f44b6c4041dcb
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2023-03-26 11:37:52 +0600

    services section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,34 +1,54 @@
 import React from 'react';
 import './Footer.css';
-import { FaFacebookF, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaFacebookF, FaGithub, FaLinkedin,} from "react-icons/fa";
 
 
 
 const Footer = () => {
     return (
-        <footer>
-           <a href="#home" className='footer_logo'>TARIQUL</a>
+      <footer>
+        <a href="#home" className="footer_logo">
+          TARIQUL
+        </a>
 
-           <ul className='permalinks'>
-           <li ><a href="#home">Home</a></li>
-           <li ><a href="#about">About</a></li>
-           <li ><a href="#experience">Experience</a></li>
-           <li ><a href="#services">Services</a></li>
-           <li ><a href="#portfolio">Portfolio</a></li>
-           <li ><a href="#Testimonials">Testimonials</a></li>
-           <li ><a href="#contact">Contact</a></li>
+        <ul className="permalinks">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          {/* <li ><a href="#Testimonials">Testimonials</a></li> */}
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
 
-           </ul>
-
-           <div className="footer_socials">
-            <a href="https://facbook.com"><FaFacebookF></FaFacebookF></a>
-            <a href="https://facbook.com"><FaLinkedin></FaLinkedin></a>
-            <a href="https://facbook.com"><FaTwitter></FaTwitter></a>
-           </div>
-           <div className="footer_copyright">
-            <small>&copy; TARIQUL Portfolio. All rights reserved.</small>
-           </div>
-        </footer>
+        <div className="footer_socials">
+          <a href="https://facbook.com">
+            <FaFacebookF></FaFacebookF>
+          </a>
+          <a href="https://facbook.com">
+            <FaLinkedin></FaLinkedin>
+          </a>
+          <a href="https://facbook.com">
+           
+            <FaGithub></FaGithub>
+          </a>
+        </div>
+        <div className="footer_copyright">
+          <small>&copy; TARIQUL Portfolio. All rights reserved.</small>
+        </div>
+      </footer>
     );
 };
 
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,11 +1,73 @@
 import React from 'react';
 import './Services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
     return (
-        <section>
-            services
-        </section>
+      <section id="services">
+        <h5>What I offer</h5>
+        <h2>Services</h2>
+
+        <div className="container services_container">
+          <article className="service">
+            <div className="service_head">
+              <h2>UI/UX Design</h2>
+            </div>
+
+            <ul className="service_list">
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </ul>
+          </article>
+          {/* End of UI/UX  */}
+          <article className="service">
+            <div className="service_head">
+              <h3>Web Development</h3>
+            </div>
+
+            <ul className="service_list">
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </ul>
+          </article>
+          {/* End of web development  */}
+          <article className="service">
+            <div className="service_head">
+              <h3>Content Creation</h3>
+            </div>
+
+            <ul className="service_list">
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <BiCheck className="service_list-icon"></BiCheck>
+              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+            </ul>
+          </article>
+          {/* End of content creation */}
+        </div>
+      </section>
     );
 };
 
